cbcf4e855e1cb854a9d498b899e707a16d427340
VinTran <tranquillo.vinnie@gmail.com>
2026-03-16 10:06:37 -0400

    New RSVP page is working

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,19 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  envPrefix: ["VITE_", "MAPS_"]
+  envPrefix: ["VITE_", "MAPS_"],
+  server: {
+    proxy: {
+      "/functions/tailgatetime-prod": {
+        target: "https://us-central1-tailgatetime-prod.cloudfunctions.net",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/functions\/tailgatetime-prod/, "")
+      },
+      "/functions/lot-legends": {
+        target: "https://us-central1-lot-legends.cloudfunctions.net",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/functions\/lot-legends/, "")
+      }
+    }
+  }
 });
